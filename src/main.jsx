29e60b051f71ef683a0520f2e75dc3781cdcbd66
sheetal <sheetalkumarilife@gmail.com>
2024-09-03 23:34:26 +0530

    vercel error

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "./App.css";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>

db164ac65bfd5356e03daf713028458acc3f118b
sheetal <sheetalkumarilife@gmail.com>
2024-09-03 23:58:59 +0530

    added css

--- src/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import { sliceReducer } from './slice.js';
-
-export const store = configureStore({
-  reducer:{
-    movieReducer : sliceReducer,
-},
-});

90f87b6fa0e547f5b9bcb5c2236488afd92f37da
sheetal <sheetalkumarilife@gmail.com>
2024-09-03 23:24:16 +0530

    vercel error fixed

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
 import { sliceReducer } from './slice.js';
 
-const store = configureStore({
+export const store = configureStore({
   reducer:{
     movieReducer : sliceReducer,
 },
 });
-
-export default store;
